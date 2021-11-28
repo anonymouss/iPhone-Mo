b62833e003e3b342466dc33df19b5e02dfec5fdc
jcong <jjcong@outlook.com>
2021-11-28 22:48:48 +0800

    add some clear hints
    Change-Id: I0eaf5319237d40cc9b4ca51e289f80260055e1f5

--- main.py
+++ main.py
@@ -1,30 +1,47 @@
 #!/usr/bin/env python3
 
 import config
-import requests, time, webbrowser, traceback, threading
+import requests, time, webbrowser, traceback, threading, os
 from wxpy import *
-from pytimedinput import timedInput, timedKey
+from pytimedinput import timedKey
+
 
 DEBUG_MODE = False
 MAX_RETRIES = 100
 receivers = []
 stop_requested = False
+hint = "Press q/Q to stop monitor\n"
+bot = None
+
 
 def logout_cb():
+    global hint
     receivers.clear()
-    input_text, timedout = timedInput("Logged out. Relogin? 1 for yes, 0 for no: ", timeout=60)
-    relogin = (input_text == '1')
-    if timedout or not relogin:
-        print("Continue running without WeChat")
-    else:
-        init_wechat()
+    hint += "WeChat logged out, you can input r/R to relogin\n"
+    print(hint)
+
+
+# just to avoid clearing the screen
+def login_cb():
+    if os.path.exists('QR.png'):
+        os.remove('QR.png')
 
 
 def init_wechat():
-    bot = Bot(logout_callback=logout_cb)
+    global hint
+    bot = Bot(cache_path=True, login_callback=login_cb, logout_callback=logout_cb)
+    print('{} login in successfully'.format(bot.self.name))
     for receiver in config.receivers:
         receivers.append(bot.friends().search(receiver)[0])
-    print(receivers)
+    if len(receivers) != 0:
+        hint = hint.split('\n')[0] + '\n'
+        print('Receivers:', end=' ')
+        for recv in receivers:
+            print(recv.name, end = ' ')
+        print()
+    else:
+        print("No matched receiver found, please check receiver info")
+
 
 def monitor():
     results = requests.get(config.check_url, headers=config.headers).json()
@@ -55,29 +72,42 @@ def monitor():
     else:
         return False
 
+
 def notify_error(error):
     for receiver in receivers:
             receiver.send('received exception: {} at {}'.format(error, now()))
+
 
 def notify_receivers(messages):
     for receiver in receivers:
             for msg in messages:
                 receiver.send(msg)
 
+
 def now():
     return time.asctime(time.localtime(time.time()))
 
 
-def user_request_exit():
+def key_event_monitor():
     global stop_requested
-    _, _ = timedKey("Press q/Q to stop monitor", allowCharacters="qQ", timeout=-1)
-    stop_requested = True
+    global hint
+    while not stop_requested:
+        print(hint)
+        input_text, _ = timedKey(timeout=-1, allowCharacters='qQrR')
+        if input_text == 'q' or input_text == 'Q':
+            stop_requested = True
+        elif (input_text == 'r' or input_text == 'R') and len(receivers) == 0:
+            init_wechat()
+        else:
+            print("I can't understand your input")
+
+
 
 if __name__ == '__main__':
     if not DEBUG_MODE:
         init_wechat()
 
-    key_thread = threading.Thread(target=user_request_exit, name='key-event-thread')
+    key_thread = threading.Thread(target=key_event_monitor, name='key-event-thread')
     key_thread.setDaemon(True)
     key_thread.start()
 
@@ -95,12 +125,14 @@ if __name__ == '__main__':
             error_count += 1
             print('Exception @ ', now())
             traceback.print_exc()
+            print(hint)
             notify_error(repr(e))
             time.sleep(60 * 5)
         if done:
             print("found @ ", now())
             if DEBUG_MODE:
                 break
+            print(hint)
             time.sleep(60 * 5);
 
     print("Exit.")
